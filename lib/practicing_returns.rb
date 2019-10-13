ff97c1fe85ec00be13c7a49393191809edc64204
Charlie Oster <oster.charlie@yahoo.com>
2019-10-13 17:24:17 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,20 @@ def hello(array)
   end
 end
 
+my_each(list) {|i| puts "So I think #{i} is junk"}
+
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
